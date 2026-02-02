c059ab43c92af1ab24d8c9220f66eb23b4b08870
leelyby <leelyby@naver.com>
2026-02-02 23:39:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const reveals = document.querySelectorAll('.reveal');
+const menuBtn = document.querySelector('.menu-btn');
+const sidebar = document.querySelector('.sidebar');
 
 window.addEventListener('scroll', () => {
   reveals.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
       el.classList.add('active');
     }
   });
+});
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
 });
